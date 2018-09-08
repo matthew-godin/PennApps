a932ed6aef6304df3330571055c656d6a1bd97aa
ke2li <li.kyle25@gmail.com>
2018-09-08 01:00:27 -0400

    Inital backend setup for google maps

---
+++ Backend/server.js
@@ -0,0 +1,36 @@
+var express = require('express')
+var app = express();
+const bodyParser = require('body-parser');
+var empty = require('is-empty');
+
+function get_no_data(){
+    return JSON.stringify({data:false});
+}
+
+async function getApts(req, res){
+    if(empty(req) || empty(req.body)) res.send(get_no_data());
+
+    let body = req.body;
+
+    let central_address = body.address;
+    let province = body.province;
+    let city = body.city;
+
+    if(empty(central_address) || empty(province) || empty(city)) res.send(get_no_data());
+
+    res.send(JSON.stringify({data:true}))
+}
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json())
+
+app.post('/api/getApts', [getApts]);
+
+app.set('port', process.env.port || 3000)
+
+app.listen(app.get('port'), () => {
+    console.log('App Started on Port: ' + app.get('port'))
+})
